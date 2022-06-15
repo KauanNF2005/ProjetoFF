e2496a5ee481311a6a037299100bf4cdaefab948
KauanNF2005 <kauanfontenele@gmil.com>
2022-06-15 10:04:18 -0300

    projeto ff

---
+++ perguntas = [.js
@@ -0,0 +1,21 @@
+
+
+perguntas = [
+    [
+        "titulo"="Fruta favorita",
+        "opcoes"=[
+            "Maçã",
+            "Banana",
+            "Pessego"
+        ]
+    ],
+    [
+        "titulo"="Jogo Favorito",
+        "opcoes"=[
+            "Super Mario",
+            "GTA",
+            "Sonic"
+        ]
+    ]
+]
+
